e48770e61cd69bb75e91a17a31e7764e3aa81899
Siphalor <Siphalor@users.noreply.github.com>
2017-04-25 14:54:00 +0200

    First real desktop support
    + Added check for mobile browser
    + Added key controls
    	* Therefore redesigned the game's look a bit (only for desktop)
    	+ Added little tutorial for key controls
    		+ Therefore added new internal tutorial item (always id 0)
    + Made it possible to use the mouse

--- kts.js
+++ kts.js
@@ -1,5 +1,8 @@
 function kts(){
 kts.version="B 1.3 P 1";
+/* Check mobile/desktop */
+desktop=!(function isMobile(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))return true;return false})(navigator.userAgent||navigator.vendor||window.opera);
+
 var $get={};
 window.location.href.replace(/[?&]([^=&]+)=?([^&]*)/gi,function(m,name,value,p,s){$get[name]=value;return m;});
 var d=window.document;d.write(". ");d.title="Keep Them Small - "+kts.version;var b=d.body;b.innerHTML="";var a=b.appendChild(d.createElement("canvas"));a.innerHTML="Please try to use a modern browser."; a.style.position="absolute";a.style.left="0";a.style.top="0";d.addEventListener("resize",resize);
@@ -66,7 +69,7 @@ o.fillText(text,0,0);
 }o.restore();};
 var data={debug:true,bg:"black",maxtps:30,ticking:false,sm:0,x:3,y:4,s:0,ms:0,rs:-1,c:[],
 lose:function(i){data.s=1.5;data.f=0.05;if(i!==undefined)data.c[i].s=3;},
-init:function(){for(let i=0;i<data.x*data.y;i++){data.c[i]={s:0,p:0,iid:-1};}data.q=15;data.su=0;data.v=1.1;data.score=0;data.f=0}};
+init:function(){let l=desktop?10:data.x*data.y;data.y=desktop?3:data.y;data.x=desktop?4:data.x;for(let i=0;i<l;i++){data.c[i]={s:0,p:0,iid:desktop?0:-1};if(desktop)modes[data.sm].items[0].onchoose(i);switch(i){case 0:data.c[i].val='E';break;case 1:data.c[i].val='R';break;case 2:data.c[i].val='T';break;case 3:data.c[i].val='S';break;case 4:data.c[i].val='D';break;case 5:data.c[i].val='F';break;case 6:data.c[i].val='G';break;case 7:data.c[i].val='X';break;case 8:data.c[i].val='C';break;case 9:data.c[i].val='V';break;}}data.q=15;data.su=0;data.v=1.1;data.score=0;data.f=0}};
 Object.defineProperty(window,"cw",{get:function(){return a.width;},set:function(v){a.width=v;}});
 Object.defineProperty(window,"ch",{get:function(){return a.height;},set:function(v){a.height=v;}});
 Object.defineProperty(data,"sc",{
@@ -87,19 +90,26 @@ ontapclose:function(i){if((data.sc%2==1)^data.c[i].logic){data.lose(i);return 0;
 ontapreopen:function(i){data.c[i].logic=!data.c[i].logic;},
 render:function(c){renderer.fillCircle(c.x,c.y,p2r(c.p),this.color);o.fillStyle="white";renderer.font(p2r(c.p));o.textAlign="center";o.textBaseline="middle";if(c.s==3)o.fillStyle="#f61";o.fillText(c.logic?"X":"O",c.x,c.y);
 }});
+var tuti=Item.apply({color:"#888",c:0,
+onchoose:function(i){data.c[i].iid=0;return false},
+onreachmin:function(i){data.c[i].iid=-1;delete data.c[i].val;},
+render:function(c){renderer.fillCircle(c.x,c.y,p2r(c.p),this.color);o.fillStyle="white";renderer.font(p2r(c.p));o.textAlign="center";o.textBaseline="middle";if(c.s==3)o.fillStyle="#f61";o.fillText(c.val,c.x,c.y);
+}});
 
-var me=new Mode("Experimental");me.setmaxp(35);me.additem(rb);me.additem(ci);me.additem(fi);me.additem(li);me.ci=0.95;
+var me=new Mode("Experimental");me.setmaxp(35);me.additem(tuti);me.additem(rb);me.additem(ci);me.additem(fi);me.additem(li);me.ci=0.95;
 me.su=75;
 me.share="I experimented a little bit with '%n' and got a score of %p.";
-var mati=new Mode("All The Items");mati.additem(rb);mati.ci=0.5;mati.additem(ci);mati.additem(fi);
+var mati=new Mode("All The Items");mati.additem(tuti);mati.additem(rb);mati.ci=0.5;mati.additem(ci);mati.additem(fi);
 mati.su=100;
 mati.share="I tried to use all the items and won '%n' with %p points!";
-var mt=new Mode("Think!");mt.additem(ci,3);mt.additem(li,95);mt.additem(rb,2);mt.ci=1;mt.su=15;mt.share="I thought I were intelligent but I failed with a score of %p playing %n";
-var mc=new Mode("Classic");mc.su=100;
+var mt=new Mode("Think!");mt.additem(tuti);mt.additem(ci,3);mt.additem(li,95);mt.additem(rb,2);mt.ci=1;mt.su=15;mt.share="I thought I were intelligent but I failed with a score of %p playing %n";
+var mc=new Mode("Classic");mc.additem(tuti);mc.su=100;
 var modes=[mc,mati,mt,me];
 data.init();
 resize();
-a.addEventListener("touchstart",touch);
+if(desktop){a.addEventListener("mousedown",function(e){touch({changedTouches:[{pageX:e.pageX,pageY:e.pageY}]})});
+d.addEventListener('keydown',key);}
+else{a.addEventListener("touchstart",touch);}
 var ti=20;
 var ticks=[(new Date()).getTime()];
 setInterval(function(){
@@ -107,7 +117,6 @@ if(ticks[ticks.length-1]+1000/data.maxtps<=(new Date()).getTime()&&ticks.length<
 },ti);
 setInterval(chooseTick,200);
 setInterval(function(){if(a.style.width!=innerWidth+"px"||a.style.height!=innerHeight+"px")resize();if(document.body.scrollTop!=0)document.body.scrollTop=0;if(document.body.scrollLeft!=0)document.body.scrollLeft=0;},1000);
-data.init();
 
 function tick(){
 data.ticking=true;
@@ -206,7 +215,7 @@ cw=innerWidth;ch=innerHeight;}
 var h=ch;
 a.style.width=innerWidth+"px";
 a.style.height=innerHeight+"px";
-if((ch>cw)!=(data.y>data.x)){
+if((ch>cw)!=(data.y>data.x)&&!desktop){
 data.x=(ch>cw?Math.min(data.x,data.y):Math.max(data.x,data.y));
 data.y=data.c.length/data.x;
 }
@@ -222,11 +231,21 @@ data.m=0.20*h/(data.y+1);
 data.r=(h-(data.y+1)*data.m)/data.y/2;}
 var rx=cw-data.x*(data.m+data.r*2)-data.m;
 var ry=h-data.y*(data.m+data.r*2)-data.m;
-for(i in data.c){
+if(desktop){
+for(i=0;i<data.c.length;i++){
+let y=i<=2?0:(i<=6?1:2);
+let width=y==1?4:3;
+let x=i-(y==1?3:(y==2?7:0));
+data.c[i].x=data.m+x*data.m+x*data.r*2+data.r+rx/2+(y==1?0:data.m/2+data.r);
+data.c[i].y=data.m+y*data.m+y*data.r*2+data.r+ry/2;
+}
+} else {
+for(i=0;i<data.c.length;i++){
 let y=Math.floor(i/data.x);
 let x=i-y*data.x;
 data.c[i].x=data.m+x*data.m+x*data.r*2+data.r+rx/2;
 data.c[i].y=data.m+y*data.m+y*data.r*2+data.r+ry/2;
+}
 }
 }
 }
@@ -308,6 +327,43 @@ if(pInC(w-d,h*0.85,d*0.6,x,y)) data.s=0;
 }
 break;
 default: break;
+}
+}
+function key(e){
+let c=e.charCode||e.keyCode;
+switch(c){
+case 69:
+modes[data.sm].ontouchcircle(0);
+break;
+case 82:
+modes[data.sm].ontouchcircle(1);
+break;
+case 84:
+modes[data.sm].ontouchcircle(2);
+break;
+case 83:
+modes[data.sm].ontouchcircle(3);
+break;
+case 68:
+modes[data.sm].ontouchcircle(4);
+break;
+case 70:
+modes[data.sm].ontouchcircle(5);
+break;
+case 71:
+modes[data.sm].ontouchcircle(6);
+break;
+case 88:
+modes[data.sm].ontouchcircle(7);
+break;
+case 67:
+modes[data.sm].ontouchcircle(8);
+break;
+case 86:
+modes[data.sm].ontouchcircle(9);
+break;
+default:
+chooseTick();
 }
 }
 function encode(s){
